4db2dd88f9badfc2c3a1fb9176a24514973b998e
Nicholas Ho <nicholas_hodj@outlook.com>
2016-10-04 17:41:44 +1100

    Initial commit

---
+++ ServerIntegTests/ResourceTests/DeviceTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ServerIntegTests
+{
+    [TestClass]
+    public class DeviceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
