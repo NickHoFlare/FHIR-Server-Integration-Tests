c32ad1648f7bd117a3e9bf58c8603a8dcd7e1a06
Nicholas Ho <nicholas_hodj@outlook.com>
2016-10-04 21:54:51 +1100

    Wrote 1 integration test for readdevice.

---
+++ ServerIntegTests/MockedResources.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hl7.Fhir.Model;
+
+namespace ServerIntegTests
+{
+    public class MockedResources
+    {
+        public static Patient patient1 = new Patient()
+        {
+            Name = new List<HumanName>()
+            {
+                new HumanName()
+                {
+                    Given = new List<string>()
+                    {
+                        "Roger"
+                    },
+                    Family = new List<string>()
+                    {
+                        "Newman"
+                    },
+                    Use = HumanName.NameUse.Official
+                }
+            },
+            Telecom = new List<ContactPoint>()
+            {
+                new ContactPoint()
+                {
+                    
+                }
+            }
+        }
+
+        public static  Device device1 = new Device()
+        {
+            Type = new CodeableConcept()
+            {
+                Coding = new List<Coding>()
+                {
+                    new Coding()
+                    {
+                        System = "http://snomed.info/sct",
+                        Code = "86184003",
+                        Display = "Electrocardiographic monitor and recorder"
+                    }
+                },
+                Text = "ECG"
+            },
+            Status = Device.DeviceStatus.Available,
+            Manufacturer = "Acme Devices, Inc",
+            Model = "AB 45-J",
+            LotNumber = "43453424"
+        };
+
+        public static Device device2 = new Device()
+        {
+            Type = new CodeableConcept()
+            {
+                Coding = new List<Coding>()
+                {
+                    new Coding()
+                    {
+                        System = "http://snomed.info/sct",
+                        Code = "27113001",
+                        Display = "Mass Measurement Apparatus"
+                    }
+                },
+                Text = "Weighing Machine"
+            },
+            Status = Device.DeviceStatus.Available,
+            Manufacturer = "Acme Devices, Inc",
+            Model = "OD 9-0",
+            LotNumber = "43453424"
+        };
+    }
+}
--- ServerIntegTests/ResourceTests/DeviceTest.cs
+++ ServerIntegTests/ResourceTests/DeviceTest.cs
@@ -1,14 +1,58 @@
-﻿using System;
+﻿using System.Net;
+using System.Net.Http;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Rest;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace ServerIntegTests
+namespace ServerIntegTests.ResourceTests
 {
     [TestClass]
     public class DeviceTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        private FhirClient fhirClient;
+        private HttpClient httpClient;
+        private const string baseUrl = "https://localhost:44384/fhir";
+
+        [TestInitialize]
+        public void TestInitialize()
         {
+            fhirClient = new FhirClient(baseUrl);
+            httpClient = new HttpClient();
+        }
+
+        [TestMethod]
+        public void TestRead()
+        {
+            // Case where all OK
+            ResourceIdentity identity1 = ResourceIdentity.Build("Patient", "1");
+            ResourceIdentity identity2 = ResourceIdentity.Build("Patient", "2");
+            ResourceIdentity identity3 = ResourceIdentity.Build("Patient", "3");
+
+            var patient1 = fhirClient.Read<Patient>(identity1);
+            var patient2 = fhirClient.Read<Patient>(identity2);
+            var patient3 = fhirClient.Read<Patient>(identity3);
+            var response1 = httpClient.GetAsync(baseUrl + "/Patient/1").Result;
+            var response2 = httpClient.GetAsync(baseUrl + "/Patient/2").Result;
+            var response3 = httpClient.GetAsync(baseUrl + "/Patient/3").Result;
+
+            Assert.AreEqual("1", patient1.Id);
+            Assert.AreEqual("2", patient2.Id);
+            Assert.AreEqual("3", patient3.Id);
+            Assert.AreEqual(HttpStatusCode.OK, response1.StatusCode);
+            Assert.AreEqual(HttpStatusCode.OK, response2.StatusCode);
+            Assert.AreEqual(HttpStatusCode.OK, response3.StatusCode);
+
+            // Case where Device does not exist
+            var response4 = httpClient.GetAsync(baseUrl + "/Patient/4").Result;
+            var response5 = httpClient.GetAsync(baseUrl + "/Patient/5").Result;
+
+            Assert.AreEqual(HttpStatusCode.NotFound, response4.StatusCode);
+            Assert.AreEqual(HttpStatusCode.NotFound, response5.StatusCode);
+        }
+
+        [TestMethod]
+        public void TestCreate()
+        {
+            fhirClient.Create()
         }
     }
-}
